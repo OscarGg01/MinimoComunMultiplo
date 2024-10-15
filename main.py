685949f3c957377a383df143fafee02b4a1a11bc
72350753 <72350753@continental.edu.pe>
2024-10-14 19:40:41 -0500

    Se modificó el código para agregar manejo de excepciones y evitar que el programa falle si el usuario ingresa un valor no numérico

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ if __name__ == '__main__':
     numeros = []
 
     for i in range(cantidadNumeros):
-        print(f"Número {i + 1:2}: ", end="", flush=True)
-        numeros.append(int(input("")))
+        while True:
+            try:
+                print(f"Número {i+1:2}: ", end="", flush=True)
+                numero = int(input(""))  # Intentar convertir la entrada a entero
+                numeros.append(numero)
+                break  # Si todo está bien, salir del bucle
+            except ValueError:
+                print("Error: Debes ingresar un número entero válido. Inténtalo de nuevo.")
 
     print(f"Números = {numeros}")
     operacionesEnteros = OperacionesEnteros(numeros)
-    print(f"MCM= {operacionesEnteros.calcularMCM()}")
+    try:
+        print(f"MCM= {operacionesEnteros.calcularMCM()}")
+    except Exception as e:
+        print(f"Error al calcular el MCM: {e}")
